46b7795c9c7669ed7fd27a11199e9db3a226430c
Olesia Tsaturian <olesiases@gmail.com>
2025-11-04 19:41:06 +0200

    made hw6

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,43 @@
-function getElementWidth(content, padding, border) {
-  const totalContent = Number.parseFloat(content);
-  const paddingTolal = Number.parseFloat(padding);
-  const borderTotal = Number.parseFloat(border);
+class StringBuilder {
+  #value;
+  constructor(initialValue = '') {
+    this.#value = initialValue;
+  }
 
-  return totalContent + 2 * paddingTolal + 2 * borderTotal;
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
+
+/*
+Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
+
+Оголоси наступні методи класу:
+
+getValue() — повертає поточне значення приватної властивості value.
+padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
+padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+*/
